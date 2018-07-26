05292c5ede978446f7e1870565da2eb210f1b1af
sumerac <41480566+sumerac@users.noreply.github.com>
2018-07-26 17:10:11 -0400

    activity B after pep8

--- PYTHON_Activity_B_VISUALIZATION_Sumera_Chaudhry.py
+++ PYTHON_Activity_B_VISUALIZATION_Sumera_Chaudhry.py
@@ -19,9 +19,9 @@ TIME = df['Time in ms']
 """PLOTTING ACCELEROMETER DATA"""
 plt.figure(1)
 plt.grid(b=True, which='both', color='y', linestyle='-')
-plt.plot(TIME/1000, ACC_X,'b')
-plt.plot(TIME/1000, ACC_Y,'g')
-plt.plot(TIME/1000, ACC_Z,'r')
+plt.plot(TIME/1000, ACC_X, 'b')
+plt.plot(TIME/1000, ACC_Y, 'g')
+plt.plot(TIME/1000, ACC_Z, 'r')
 plt.xlabel('Xaxis --> Time_ms')
 plt.ylabel('Yaxis --> Accelerometer m/s^2')
 plt.title('Sensor data for Accelerometer Vs Time')
@@ -32,25 +32,25 @@ plt.show()
 """PLOTTING ACCERELOMETER DATA IN SUBPLOTS"""
 f, axes = plt.subplots(3, 1)
 """SUBPLOT 1"""
-axes[0].plot(TIME/1000, ACC_X,'g')
+axes[0].plot(TIME/1000, ACC_X, 'g')
 axes[0].set_ylabel('Accelerometer X m/s^2')
 axes[0].set_title('Accelerometer XYZ(m/s^2) vs Time')
 axes[0].grid(b=True, which='both', color='r', linestyle='-')
 axes[0].legend(loc='best', shadow=True, fontsize='small')\
-.get_frame().set_facecolor('#99ffdd')
+               .get_frame().set_facecolor('#99ffdd')
 """SUBPLOT 2"""
-axes[1].plot(TIME/1000, ACC_Y,'r')
+axes[1].plot(TIME/1000, ACC_Y, 'r')
 axes[1].set_ylabel('Accelerometer Y m/s^2')
 axes[1].grid(b=True, which='both', color='b', linestyle='-')
 axes[1].legend(loc='best', shadow=True, fontsize='small')\
-.get_frame().set_facecolor('#ff99e6')
+               .get_frame().set_facecolor('#ff99e6')
 """SUBPLOT 3"""
-axes[2].plot(TIME/1000, ACC_Z,'k')
+axes[2].plot(TIME/1000, ACC_Z, 'k')
 axes[2].set_xlabel('Time (secs)')
 axes[2].set_ylabel('Accelerometer Z m/s^2')
 axes[2].grid(b=True, which='both', color='g', linestyle='-')
 axes[2].legend(loc='best', shadow=True, fontsize='small')\
-.get_frame().set_facecolor('#ffff4d')
+               .get_frame().set_facecolor('#ffff4d')
 
 """PLOTTING SPEED VS TIME"""
 plt.figure()
@@ -76,7 +76,7 @@ plt.show()
 
 """CHECKING FOR ANY OVERSPEEDING AND PRINTING WARNING ALERT"""
 speed_mph = [0]
-for row in range(1,len(SPEED)):
+for row in range(1, len(SPEED)):
     speed_mph.append(float(SPEED[row])*0.6214)
     if (speed_mph[row] > 42.6):
         speed_mph = speed_mph
