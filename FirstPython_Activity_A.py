4a54fc322315e4f1992ade5b4e271ab671106f30
sumerac <41480566+sumerac@users.noreply.github.com>
2018-07-26 14:34:20 -0400

    updated after pep8

--- FirstPython_Activity_A.py
+++ FirstPython_Activity_A.py
@@ -9,13 +9,17 @@ Created on Mon Jul 23 13:25:49 2018
 
 KM_TO_MILES = 0.621371
 
+
 def show_km2mi(Distance):
+
     miles = Distance * KM_TO_MILES
     print('{} kilometers converts to {} miles'.format(Distance, miles))
 
+
 def main():
+
     Distance = float(input
-                     ("Please enter the distance in kilometers that you wish" 
+                     ("Please enter the distance in kilometers that you wish"
                       "to convert in miles: "))
     show_km2mi(Distance)
 
